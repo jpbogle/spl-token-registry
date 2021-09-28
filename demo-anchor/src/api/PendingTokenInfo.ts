b742a0963c97137b1376b2118fb16d35d581a865
Jeremy Bogle <jbogle@apple.com>
2021-09-27 23:50:06 -0700

    Remove anchor program

--- demo-anchor/src/api/PendingTokenInfo.ts
+++
@@ -1,9 +0,0 @@
-import {  PublicKey } from '@solana/web3.js';
-import { TokenInfo } from './TokenInfo';
-import * as anchor from '@project-serum/anchor';
-
-export type PendingTokenInfo = {
-  tokenInfo: TokenInfo,
-  votes: anchor.BN,
-  expiration: anchor.BN,
-}
--- demo-anchor/src/api/TokenInfo.ts
+++
@@ -1,9 +0,0 @@
-import {  PublicKey } from '@solana/web3.js';
-
-export type TokenInfo = {
-  splTokenProgramAddress: PublicKey,
-  tokenName: string,
-  tokenSymbol: string,
-  tokenImageUrl: string,
-  tags: Array<string>,
-}
